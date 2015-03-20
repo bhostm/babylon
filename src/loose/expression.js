bhostm/babylon

commit 790aeb72b670d90ab4d401ed1cfa13a223317f46

Make the loose parser's parseIdent actually returns an identifier with a name

src/loose/expression.js
@@ -399,9 +399,10 @@ lp.parsePropertyAccessor = function() {
 }
 
 lp.parseIdent = function() {
+  let name = this.tok.type === tt.name ? this.tok.value : this.tok.type.keyword
+  if (!name) return this.dummyIdent()
   let node = this.startNode()
-  node.name = this.tok.type === tt.name ? this.tok.value : this.tok.type.keyword
-  this.next()
+  node.name = name
   return this.finishNode(node, "Identifier")
 }
 

commit c31dc322556d66d93819188fbc7403a32e1d2f1f

Fix infinite loop introduced by 790aeb7

src/loose/expression.js
@@ -402,6 +402,7 @@ lp.parseIdent = function() {
   let name = this.tok.type === tt.name ? this.tok.value : this.tok.type.keyword
   if (!name) return this.dummyIdent()
   let node = this.startNode()
+  this.next()
   node.name = name
   return this.finishNode(node, "Identifier")
 }